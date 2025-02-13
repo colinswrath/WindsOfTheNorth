54c30ed598f7217b95df318bba45598de3651d92
Colin <colinkelley30@gmail.com>
2025-02-12 20:43:36 -0500

    scroll fix

--- public/1.0.0/js/script.js
+++ public/1.0.0/js/script.js
@@ -2,14 +2,26 @@
   var toggle = document.querySelector('.sidebar-toggle');
   var sidebar = document.querySelector('#sidebar');
   var checkbox = document.querySelector('#sidebar-checkbox');
+  let scrollPosition = 0;
 
   document.addEventListener('click', function(e) {
     var target = e.target;
     if (target === checkbox || target === toggle) {
+      
+      if(target === toggle){
+        scrollPosition = window.scrollY;
+      }
+
       checkbox.checked = !checkbox.checked;
+      window.scrollTo({
+        top: scrollPosition,
+        behavior: 'auto'
+      });
+ 
       return;
     }
-
+    
     if (!checkbox.checked || sidebar.contains(target)) return;
   }, false);
+
 })(document);
